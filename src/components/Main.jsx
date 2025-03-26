5fd0f37cd702514229ceaadc13f618b851bf3577
alexciccarese <alex.ciccarese@gmail.com>
2025-03-26 20:47:08 +0100

    implement article addition functionality in Main component; pass addArticle function to Form component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
 import articles from "../data/articles"
 import ArticlesList from "./articleList"
 import Form from "./Form"
 
 export default function Main() {
+  const [articles, setArticles] = useState(articlesData)
+
+  function addArticle(title) {
+
+    const newArticle = {
+      id: articles.length + 1,
+      title: title,
+    }
+    
+    setArticles([...articles, newArticle])
+  }
+
+
   return (
     <>
       <h1>ARTICLES</h1>
-      <Form />
+      <Form addArticle={addArticle}/>
       <ArticlesList articles={articles}/>
     </>
   )
